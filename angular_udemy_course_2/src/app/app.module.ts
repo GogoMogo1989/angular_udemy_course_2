32af9c8c3b48e13ec0a68c61c4e0e09dfc1a5167
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-10-10 12:19:43 +0200

    20231010

--- angular_udemy_course_2/src/app/app.module.ts
+++ angular_udemy_course_2/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.c
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-ite
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeItemComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
